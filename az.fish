25140d6f61790d8ae8a19b991d0c877be4c139d9
Matt Pasco <mapasco@microsoft.com>
2021-05-10 17:29:14 -0600

    fixing parsing

--- az.fish
+++ az.fish
@@ -1,9 +1,12 @@
-function fsh_az_complete
-    set args $argv
-    $args --help  2> /dev/null \
-        | grep -e '^    .*:' \
-        | sed -e 's/ //g' \
-        | cut -d : -f 1
+function fsh_az_complete_commands
+    set args (string trim -r $argv)
+    set res (az $args[2..] --help  2> /dev/null \
+        | egrep '^[[:space:]]{4}[a-zA-Z\-]{1,}.*[[:space:]]{1,}:' \
+        | sed -e 's/^    //g' \
+        | cut -d ' ' -f 1)
+    for i in $res[2..];
+        echo $i
+    end
 end
 
-complete -c az -d "Command" -f -a '(fsh_az_complete (commandline -co))'
+complete -c az -d "Command" -f -a '(fsh_az_complete_commands (commandline -co))'
